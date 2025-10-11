dd57cf7e43c09590dc5e870dde95eaa31a41111d
Mansi Jogani <your-email@example.com>
2025-10-11 21:54:20 +0530

    Add Binary Search algorithm in C++

--- python/Polar Coordinates.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import cmath
-z= complex(input().strip())
-r=abs(z)
-theta=cmath.phase(z)
-
-print(f"{r:.3f}")
-print(f"{theta:.3f}")
--- strongPasswordValidation.js
+++
@@ -1,4 +0,0 @@
-const isStrongPassword = (string) => {
-  let regex = ^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$
-  return regex.test(string)
-}
